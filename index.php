d73bd3f01247b2a8fd77bbac715db118282c994e
Carlos Linares <cilp2912@gmail.com>
2016-11-03 21:18:58 -0600

    Index.php modified for allow only localhost request

--- index.php
+++ index.php
@@ -17,8 +17,35 @@
   
   unset($context);*/
 
+  $msg = "";
+
+  if ($_SERVER['SERVER_ADDR'] != $_SERVER['REMOTE_ADDR']){
+    http_response_code(404);
+    exit;
+  } else {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+      $msg = json_encode(
+        array(
+          'name' => $_POST['name'],
+          'email' => $_POST['email'],
+          'password' => $_POST['pass'],
+          'phone' => $_POST['phone'],
+          'company' => $_POST['company'],
+        )
+      );
+    } else {
+      $msg = $_SERVER['REQUEST_METHOD'] . $_SERVER['PATH_INFO'];
+    }
+
+   
+    http_response_code(200);
+  }
+
   $head = file_get_contents("views/header.html");
   $foot = file_get_contents("views/footer.html");
   $main = file_get_contents("views/main.html");
+  $form = file_get_contents("views/form.html");
 
-  echo $head . $main . $foot;
+  echo $head . $main . "<p><b>$msg</b></p>". $form . $foot;
